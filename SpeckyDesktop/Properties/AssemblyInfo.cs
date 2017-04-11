98734710306b429818a27414e608e8e882224f18
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2017-04-11 17:22:47 -0400

    Updated version

--- SpeckyDesktop/Properties/AssemblyInfo.cs
+++ SpeckyDesktop/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SpeckyDesktop")]
-[assembly: AssemblyDescription("Reactive model package to help develop MVVM / reactive oriented models")]
+[assembly: AssemblyDescription("Reactive model package to help develop MVVM / reactive oriented models. Michael Puckett, II 'I love, praise, and worship God'")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Triple G3")]
 [assembly: AssemblyProduct("SpeckyDesktop")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
