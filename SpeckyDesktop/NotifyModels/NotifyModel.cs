2dd449ec5a330faedf46cc6932031d41b190c832
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2017-04-10 22:38:15 -0400

    Init

---
+++ SpeckyDesktop/NotifyModels/NotifyModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Specky.NotifyModels
+{
+    abstract public class NotifyModel : INotifyPropertyChanged
+    {
+        [field: NonSerialized()]
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Automatically calls the property changed event passing the method name used to make the call unless propertyName is specified.
+        /// Note: If used inside a property propertyName can only be blank when Notify is called from directly within the property.
+        /// If the logic is passed to anoher method then propertyName must still be specified in order fire PropertyChanged with the correct property name.
+        /// </summary>
+        /// <param name="propertyName">The name of the property invoking PropertyChanged.</param>
+        public void Notify([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ SpeckyDesktop/NotifyModels/SingleInstance/SingleInstanceNotifyModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using static Specky.Constants;
+
+namespace Specky.NotifyModels.SingleInstance
+{
+    abstract public class SingleInstanceNotifyModel<T> : NotifyModel where T : class
+    {
+        protected SingleInstanceNotifyModel()
+        {
+            if (typeof(T).GetConstructors(AllBindingFlags).Where(contructorInfo => !contructorInfo.IsPrivate).Any())
+                throw new FormatException($"{typeof(T).Name} can only have a parameterless private constuctor in order to inherit from {nameof(SingleInstanceNotifyModel<T>)}");
+        }
+
+        static public T Instance { get; } = (T)typeof(T).GetConstructors(AllBindingFlags)
+                                                        .Where(contructorInfo => contructorInfo.IsPrivate)
+                                                        .FirstOrDefault()?
+                                                        .Invoke(null);
+    }
+}
